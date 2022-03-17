aaba4017f7e4afbc4902aa5743680882b701f008
Holmkvists <adamholmkvists@gmail.com>
2022-03-17 17:54:02 +0100

    small changes

--- utils.js
+++ utils.js
@@ -1,9 +1,9 @@
 const crypto = require("crypto");
 
-const getHashedPassword = (password) => {
+function getHashedPassword(password) {
   const sha256 = crypto.createHash("sha256");
   const hash = sha256.update(password).digest("base64");
   return hash;
-};
+}
 
 module.exports = { getHashedPassword };
